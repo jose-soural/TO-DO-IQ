4c76b0dff2170f1a0571dec26c7c9828d7d88782
José <soural.josef.2003@gmail.com>
2024-08-12 08:00:07 +0200

    Established better idea plan

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-name = "y"
-print(f'Task "{name}" succesfully created!')
+import json
+from os import path
+from os import remove
+from datetime import date, timedelta
+import heapq
+today = date.today()
+due = []
+overdue = []
+sleepers = []
+finished_today = []
+displayed_list = []
+
+
+def __to_date(days):
+    delta = timedelta(days)
+    return today + delta
+
+x = __to_date(-1089)
+if x < today:
+    print("Oh, no no")
--- pg3.py
+++ pg3.py
@@ -1,13 +1,15 @@
 import json
 from os import path
 from os import remove
-from datetime import date
-date_today = date.today()
+from datetime import date, timedelta
+import heapq
+today = date.today()
 due = []
 overdue = []
-sleepers = [None]
+sleepers = []
 finished_today = []
-current_file = None
+displayed = None
+displayed_list = []
 
 
 def __create_file(frequency):
@@ -35,8 +37,15 @@ def __push_file(frequency, contents):
         json.dump(contents, f)
 
 
+def __display_list(task_list):
+    i = 1
+    while i < (len(task_list) + 1):
+        print(f'{i})', task_list[i-1]["name"], sep="   ")
+        i += 1
+
+
 def create_task(name, frequency="once", description="", status="unfinished"):
-    new_task = {"name": name, "description": description, "status": status}
+    new_task = {"name": name, "frequency": frequency, "description": description, "status": status}
     temp_list = __pull_file(frequency)
     temp_list.append(new_task)
     with open(f'{frequency}.txt', "w", encoding="utf-8") as f:
@@ -46,10 +55,38 @@ def create_task(name, frequency="once", description="", status="unfinished"):
     print(f'Task "{name}" successfully created!')
 
 
-def __display_tasks(frequency, status="all"):
-    temp = __pull_file(frequency)
-    if status == "all":
-        output = temp
-    else:
-        output = [item for item in temp if item["status"] == status]
-    return output
+def delete_task(task_index):
+    task = displayed_list[task_index - 1]
+    freq = task["frequency"]
+    temp = __pull_file(freq)
+    temp.remove(task)
+    __push_file(freq, temp)
+
+    displayed_list.pop(task_index - 1)
+    __display_list(displayed_list)
+
+
+def __to_date(days):
+    delta = timedelta(days)
+    return today + delta
+
+
+def sleep_task(task_index, duration=0, until_date=today):
+    duration = int(duration)
+    if until_date <= today:
+        if duration <= 0:
+            print('Error: Please provide at least one valid attribute between "duration" or "until_date".')
+            return
+        else:
+            until_date = __to_date(duration)
+    task = displayed_list[task_index - 1]
+    freq = task["frequency"]
+    temp = __pull_file(freq)
+    temp_task = temp[temp.index(task)]
+    temp_task["status"] = "asleep"
+    temp_task["until"] = until_date
+    __push_file(freq, temp)
+
+
+# displayed_list.pop(task_index - 1)
+# __display_list(displayed_list)
